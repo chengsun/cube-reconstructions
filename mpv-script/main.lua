f7a8da11ebfc764fd562ae32bc47623dd80f7e42
Cheng Sun <_@chengsun.uk>
2022-01-19 23:45:20 +0000

    render cube net wip

--- mpv-script/main.lua
+++ mpv-script/main.lua
@@ -14,6 +14,7 @@ local state =
     label_filename,
     label_file,
     mode,
+    sticker_cursor,
     events, -- { {time=3,moves={"U","D"},permutation={},stickers={}}, ...}. permutation[sticker_id] = net_id where that sticker is right now
   }
 
@@ -26,6 +27,7 @@ function state_reset()
   end
   state.label_file = nil
   state.mode = MODE_EDIT_MOVES
+  state.sticker_cursor = 23
   state.events = {{time = -1, moves = {}, permutation = cubelib.Permutation.new(), stickers = {}}}
 end
 
@@ -110,7 +112,9 @@ function events_moves_append(move)
   while i <= #state.events do
     local permutation = state.events[i - 1].permutation
     for _, move in ipairs(state.events[i].moves) do
-      permutation = permutation * cubelib.Permutation.of_move_string(move)
+      local p2 = cubelib.Permutation.of_move_string(move)
+      assert(p2 ~= nil)
+      permutation = permutation * p2
     end
     state.events[i].permutation = permutation
     i = i + 1
@@ -118,7 +122,7 @@ function events_moves_append(move)
 end
 
 local keymap = {}
-function add_keystring(name, str)
+local function add_keystring(name, str)
   for i = 1, #str do
     local c = str:sub(i,i)
     keymap[c] = keymap[c] or {}
@@ -128,53 +132,92 @@ function add_keystring(name, str)
   msg.info(utils.to_string(keymap))
 end
 
-function escape_ass(s)
+local function escape_ass(s)
   return s:gsub("\\n", " "):gsub("\\$", ""):gsub("{","\\{")
+end
+
+local _face_net_position_of_face_id = {{1, 2}, {0, 1}, {1, 1}, {2, 1}, {3, 1}, {1, 0}}
+local _face_id_of_face_net_position = {}
+for face_id, face_net_position in ipairs(_face_net_position_of_face_id) do
+  _face_id_of_face_net_position[face_net_position[2] * 4 + face_net_position[1] + 1] = face_id
+end
+local function face_net_position_of_face_id(face_id)
+  local p = _face_net_position_of_face_id[face_id]
+  return p[1], p[2]
+end
+local function face_id_of_face_net_position(x, y)
+  if x < 0 or x >= 4 then return nil end
+  return _face_id_of_face_net_position[y * 4 + x + 1]
+end
+for i = 1, 6 do
+  local x, y = face_net_position_of_face_id(i)
+  assert(i == face_id_of_face_net_position(x, y))
 end
 
 function rerender()
   msg.trace("rerender")
 
   -- debug info
-  state.osd.data =
-    "\n{\\pos(0,240)\\fnMonospace\\fs10\\q1\\bord2\\c&HB0B0B0&}mode: " ..
-    utils.to_string(state.mode) ..
-    "\\Nevents: " ..
-    escape_ass(utils.to_string(state.events))
+  local ass_debug = assdraw.ass_new()
+  ass_debug:pos(0, 240)
+  ass_debug:append(
+    "{\\fnMonospace\\fs10\\q1\\bord2\\c&HB0B0B0&}"
+  )
+  ass_debug:append(string.format("mode: %s\\Nevents: %s",
+                                 utils.to_string(state.mode),
+                                 escape_ass(utils.to_string(state.events))))
 
+  local ass_moves = assdraw.ass_new()
+  local ass_sticker_cursor = assdraw.ass_new()
+  local ass_stickers = assdraw.ass_new()
   if state.playback_time then
     local idx = binary_search_last_le(state.events, state.playback_time)
 
     assert(idx >= 1 and idx <= #state.events)
 
     -- moves
-    state.osd.data = state.osd.data .. "\n{\\pos(0,0)}"
-    if state.events[idx] then
-      if state.events[idx].time == state.playback_time then
-        state.osd.data = state.osd.data .. "{\\c&HFFFF00&}"
-      else
-        state.osd.data = state.osd.data .. "{\\c&HB0B0B0&}"
-      end
-      state.osd.data = state.osd.data .. table.concat(state.events[idx].moves, " ") .. " |"
+    ass_moves:pos(0, 0)
+    if state.events[idx].time == state.playback_time then
+      ass_moves:append("{\\c&HFFFF00&}")
+    else
+      ass_moves:append("{\\c&HB0B0B0&}")
     end
+    ass_moves:append(table.concat(state.events[idx].moves, " "))
+    ass_moves:append(" |")
 
     -- stickers
-    local face_net_position_of_face_id = {{1, 2}, {0, 1}, {1, 1}, {2, 1}, {3, 1}, {1, 0}}
     for sticker_id = 1, 54 do
       local net_id = state.events[idx].permutation[sticker_id]
       local net_face_id = cubelib.face_id_of_net_id(net_id)
       local net_face_local_id = cubelib.face_local_id_of_net_id(net_id)
       local net_face_local_x, net_face_local_y = cubelib.face_local_coord_of_face_local_id(net_face_local_id)
-      local net_face_net_position = face_net_position_of_face_id[net_face_id]
-      state.osd.data =
-        state.osd.data ..
-        string.format("\n{\\pos(%d,%d)\\fs10}%d",
-                      640 + 15 * ((net_face_net_position[1] - 4) * 4 + net_face_local_x),
-                      15 * ((2 - net_face_net_position[2]) * 4 + 3 - net_face_local_y),
-                      sticker_id)
+      local net_face_net_x, net_face_net_y = face_net_position_of_face_id(net_face_id)
+      local screen_x = 640 + 15 * ((net_face_net_x - 4) * 4 + net_face_local_x)
+      local screen_y = 15 * ((2 - net_face_net_y) * 4 + 3 - net_face_local_y)
+      if net_id == state.sticker_cursor then
+        ass_sticker_cursor:append("{\\c&H00FFFF&\\alpha&H80&\\bord0}")
+        ass_sticker_cursor:draw_start()
+        ass_sticker_cursor:rect_cw(screen_x - 7, screen_y - 7, screen_x + 7, screen_y + 7)
+        ass_sticker_cursor:draw_stop()
+      end
+      ass_stickers:new_event()
+      ass_stickers:pos(screen_x, screen_y)
+      ass_stickers:append("{\\fs10\\an5}")
+      ass_stickers:append(string.format("%d", sticker_id))
     end
   end
 
+  local ass = assdraw.ass_new()
+  ass:append(ass_debug.text)
+  ass:new_event()
+  ass:append(ass_moves.text)
+  ass:new_event()
+  ass:append(ass_sticker_cursor.text)
+  ass:new_event()
+  ass:append(ass_stickers.text)
+
+  state.osd.data = ass.text
+  state.osd.z = 500
   state.osd:update()
 end
 
